476eb979c9d2da8785d19e1024fe7696d127106e
Marc Lejay <lejay.marc@gmail.com>
2015-04-01 21:40:14 -0700

    Added gate primitives

---
+++ cells/and.cmd
@@ -0,0 +1,14 @@
+stepsize 50
+w A B AandB
+vector in A B
+set in 00
+s
+set in 01
+s
+set in 10
+s
+set in 11
+s
+set in 00
+s
+analyzer AandB A B
---
+++ cells/nor.cmd
@@ -0,0 +1,12 @@
+stepsize 50
+w A B Out
+vector in A B
+set in 00
+s
+set in 01
+s
+set in 10
+s
+set in 11
+s
+analyzer Out A B
